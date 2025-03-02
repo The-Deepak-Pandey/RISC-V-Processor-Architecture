0331c0512ffddc362643ce4e0a34b78c3938587e
Manas <manas.inamdar@students.iiit.ac.in>
2025-03-02 18:04:40 +0530

    added PC_adder

--- SEQ/PC_adder.v
+++ SEQ/PC_adder.v
@@ -1,10 +1,10 @@
 module PC_adder (
-    input logic clk, reset,           // Clock and reset signals
-    input logic ALU_zero,             // Zero flag from ALU
-    input logic [63:0] imm_gen_out,   // Immediate generator output
-    input logic branch,               // Branch control signal
-    input logic [31:0] pc_in,         // Current PC value
-    output logic [31:0] pc_out        // Updated PC value
+    input clk, reset,           // Clock and reset signals
+    input ALU_zero,             // Zero flag from ALU
+    input [63:0] imm_gen_out,   // Immediate generator output
+    input branch,               // Branch control signal
+    input [31:0] pc_in,         // Current PC value
+    output [31:0] pc_out        // Updated PC value
 );
 
     always_ff @(posedge clk or posedge reset) begin
