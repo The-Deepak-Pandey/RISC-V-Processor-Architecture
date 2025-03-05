ae8af582dac0940bffa5be2e54b30ff4e8909fd3
its-gg14 <gautam.gandhi@students.iiit.ac.in>
2025-03-06 01:45:42 +0530

    creating hazard det unit

--- Pipeline/src/hazard_detection_unit.v
+++ Pipeline/src/hazard_detection_unit.v
@@ -1,5 +1,19 @@
 module hazard_det_unit (
-    
+    input wire idex_memRead,
+    input wire rs1_d1,
+    input wire rs2_d1,
+    input wire rd_d2,
+    output wire ifid_write,
+    output wire PC_write
 );
+
+    assign ifid_write = 1'b1;
+    assign PC_write = 1'b1;
+
+    // Check for data hazards
+    assign ifid_write = ((idex_memRead) & ((rs1_d1 == rd_d2) || (rs2_d1 == rd_d2))) ? 1'b0 : 1'b1;
+
+    // Check for control hazards
+    assign PC_write = ((idex_memRead) & ((rs1_d1 == rd_d2) || (rs2_d1 == rd_d2))) ? 1'b0 : 1'b1;
 
 endmodule
